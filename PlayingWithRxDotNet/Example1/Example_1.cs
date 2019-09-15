c0f25a51ccd072e0071aebdcd0910d73dbccce2a
Balázs Keresztúri <19balazs86@gmail.com>
2019-09-15 16:28:37 +0100

    Example #1: Refactor to use ActionBlock.

--- PlayingWithRxDotNet/Example1/Example_1.cs
+++ PlayingWithRxDotNet/Example1/Example_1.cs
@@ -2,23 +2,38 @@
 using System.Reactive.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
 
 namespace PlayingWithRxDotNet.Example1
 {
-  public class Example_1
+  public static class Example_1
   {
+    // You can set the MaxDegreeOfParallelism with an option parameter.
+    private static readonly ActionBlock<MonitorChangeState> _actionBlock
+      = new ActionBlock<MonitorChangeState>(processChangeStateAsync);
+
     public static async Task DoItAsync(CancellationToken cancellationToken)
     {
+      // ForEachAsync method is not meant to handle async methods.
+
+      //using (MonitorDirectory monitor = new MonitorDirectory("monitor"))
+      //{
+      //  await monitor.ChangeObservable.ForEachAsync(
+      //    async (MonitorChangeState state) => await processChangeStateAsync(state), cancellationToken);
+      //}
+
       using (MonitorDirectory monitor = new MonitorDirectory("monitor"))
       {
-        Task task = monitor.ChangeObservable.ForEachAsync(
-          async (MonitorChangeState state) =>
-          {
-            await processChangeStateAsync(state);
-          }, cancellationToken);
-
-        await task;
+        try
+        {
+          await monitor.ChangeObservable.ForEachAsync(x => _actionBlock.Post(x), cancellationToken);
+        }
+        catch (OperationCanceledException) { }
       }
+
+      _actionBlock.Complete();
+
+      await _actionBlock.Completion;
     }
 
     private static Task processChangeStateAsync(MonitorChangeState state)
